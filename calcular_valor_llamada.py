fe24b90648a6e24a1273e8a8e5831d3aa6e60e21
Nicolas942 <nicolascabrera@colegioguanenta.edu.co>
2025-01-31 08:56:27 -0500

    Actualización

--- calcular_valor_llamada.py
+++ calcular_valor_llamada.py
@@ -1,10 +1,10 @@
 cant_minutos = int(input("Digite la cantidad de minutos que duro la llamada ")) #input
 
 if cant_minutos <= 3:
-    print('el valor de la llama son 300 pesos')
+    print('el valor de la llamada son 300 pesos')
 
 else:
     valor_llamada = 300+50*(cant_minutos-3)
 
 
-print(f'el valor de la llama son {valor_llamada} pesos')
+print(f'el valor de la llamada son {valor_llamada} pesos')
