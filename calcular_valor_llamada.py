a24201d6b9f2d15f814fc15bd272d36040f93317
Nicolas942 <nicolascabrera@colegioguanenta.edu.co>
2025-01-31 08:30:21 -0500

    Actualización

--- calcular_valor_llamada.py
+++ calcular_valor_llamada.py
@@ -4,7 +4,7 @@ if cant_minutos <= 3:
     print('el valor de la llama son 300 pesos')
 
 else:
-    valor_llama = 300+50*(cant_minutos-3)
+    valor_llamada = 300+50*(cant_minutos-3)
 
 
-print(f'el valor de la llama son {valor_llama} pesos')
+print(f'el valor de la llama son {valor_llamada} pesos')
